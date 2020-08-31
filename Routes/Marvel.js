2f867b2eff45ec3b4df5dab500724b4545b2d8ed
Chagas-Thiago <62478250+Chagas-Thiago@users.noreply.github.com>
2020-08-31 11:04:46 +0200

    message

--- Routes/Marvel.js
+++ Routes/Marvel.js
@@ -4,18 +4,18 @@ const axios = require("axios");
 const md5 = require("md5");
 const uid2 = require("uid2");
 
-// router.get("/characters", async (req, res) => {
-//   const ts = uid2(8);
-//   const hash = md5(
-//     ts + process.env.MARVEL_SECRET_API_KEY + process.env.MARVEL_PUBLIC_API_KEY
-//   );
-//   const offset = req.query.offset;
+router.get("/characters", async (req, res) => {
+  const ts = uid2(8);
+  const hash = md5(
+    ts + process.env.MARVEL_SECRET_API_KEY + process.env.MARVEL_PUBLIC_API_KEY
+  );
+  const offset = req.query.offset;
 
-//   const response = await axios.get(
-//     `http://gateway.marvel.com/v1/public/characters?limit=200&offset=${offset}&ts=${ts}&apikey=${process.env.MARVEL_PUBLIC_API_KEY}&hash=${hash}`
-//   );
-//   res.json(response.data);
-// });
+  const response = await axios.get(
+    `http://gateway.marvel.com/v1/public/characters?limit=200&offset=${offset}&ts=${ts}&apikey=${process.env.MARVEL_PUBLIC_API_KEY}&hash=${hash}`
+  );
+  res.json(response.data);
+});
 
 router.get("/comics", async (req, res) => {
   const ts = uid2(8);
